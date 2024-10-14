cf87e40263f474d60f7541a319b5fc5e2fca86f6
Ragulvishnu15 <ragulvishnu.r@gmail.com>
2024-10-14 15:52:06 +0530

    Add files via upload

---
+++ src/pages/contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Contact = () => {
+  return <div>
+     <div className="col-lg-3 col-md-6">
+                            <div className="footer-item">
+                                <h4 className="text-light mb-3">Contact</h4>
+                                <p>Address: xyzz 48247</p>
+                                <p>Email: Example@gmail.com</p>
+                                <p>Phone: +0123 4567 8910</p>
+                                <p>Payment Accepted</p>
+                               
+                            </div>
+                        </div>
+  </div>;
+};
